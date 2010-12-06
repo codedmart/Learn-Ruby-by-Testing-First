52e80a360ab08727ba372f6429f74ca5622d033e
Brandon Martin <bmartin@zyphmartin.com>
2010-12-06 06:19:08 -0800

    made lesson1 tests pass

--- lesson1_operators.rb
+++ lesson1_operators.rb
@@ -2,6 +2,29 @@ require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 class LearnOperators
 	#add code here to make tests pass
+  def not(a)
+    a!= a
+  end
+
+  def integer_division(a, b)
+    a / b
+  end
+
+  def division(a, b)
+    a.to_f / b.to_f
+  end
+
+  def remainder(a, b)
+    a%b
+  end
+
+  def combine(a, b)
+    a + b
+  end
+
+  def subtract(a, b)
+    a - b
+  end
 end
 
 describe LearnOperators do
@@ -14,24 +37,24 @@ describe LearnOperators do
 	end
 
 	it "do integer division" do
-		skip "make this pass"
+		#skip "make this pass"
 		@operators.integer_division(5, 2).must_equal 2
 	end
 
 	describe "division" do
 
 		it "float and integer" do
-			skip "make this pass"					
+			#skip "make this pass"					
 			@operators.division(5.0, 2).must_equal 2.5
 		end
 
 		it "integer and float" do
-			skip "make this pass"					
+			#skip "make this pass"					
 			@operators.division(5, 2.0).must_equal 2.5
 		end
 
 		it "float and float" do
-			skip "make this pass"					
+			#skip "make this pass"					
 			@operators.division(5.0, 2.0).must_equal 2.5
 		end
 
@@ -40,12 +63,12 @@ describe LearnOperators do
 	describe "remainder" do
 
 		it "return remainder a" do
-			skip "make this pass"
+			#skip "make this pass"
 			@operators.remainder(5, 2).must_equal 1
 		end
 
 		it "return remainder b" do
-			skip "make this pass"
+			#skip "make this pass"
 			@operators.remainder(2, 5).must_equal 2
 		end
 
@@ -54,17 +77,17 @@ describe LearnOperators do
 	describe "combine (+)" do
 
 		it "numbers" do
-			skip "make this pass"
+			#skip "make this pass"
 			@operators.combine(2, 5).must_equal 7
 		end
 
 		it "strings" do
-			skip "make this pass"
+			#skip "make this pass"
 			@operators.combine("hello", " world").must_equal "hello world"
 		end
 
 		it "arrays" do
-			skip "make this pass"
+			#skip "make this pass"
 			@operators.combine([1,2,3], [4,5,6]).must_equal [1,2,3,4,5,6]
 		end
 
@@ -72,12 +95,12 @@ describe LearnOperators do
 
 	describe "subtraction (-)" do
 		it "of numbers" do
-			skip "make this pass"
+			#skip "make this pass"
 			@operators.subtract(10,3).must_equal 7
 		end
 
 		it "of arrays" do
-			skip "make this pass"
+			#skip "make this pass"
 			@operators.subtract([1,2,3,4,5],[4,5]).must_equal [1,2,3]
 		end
 		
